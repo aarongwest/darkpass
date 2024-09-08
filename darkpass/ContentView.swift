1ae2cef098db6dd08a4c5c7f7193ce4382ae6548
Aaron West <aaron@oshify.com>
2024-09-07 20:48:19 -0600

    Initial Commit

---
+++ darkpass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  darkpass
+//
+//  Created by Aaron West on 9/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ darkpass/darkpassApp.swift
@@ -0,0 +1,17 @@
+//
+//  darkpassApp.swift
+//  darkpass
+//
+//  Created by Aaron West on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct darkpassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
